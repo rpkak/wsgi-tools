5e9c9c6d4508344f48cd308f97b2f48dfc04867b
rpkak <rpkak@users.noreply.github.com>
2021-12-25 10:03:48 +0100

    better routing

--- wsgi_tools/routing.py
+++ wsgi_tools/routing.py
@@ -1,49 +1,82 @@
 from wsgi_tools.error import HTTPException
 
 
-def match_pattern(pattern, test):
+def _check_path(router, environ, value):
     args = []
     string = True
-    for i in range(len(pattern)):
+    path = environ['PATH_INFO']
+    for i in range(len(value)):
         if string:
-            if test[:len(pattern[i])] == pattern[i]:
-                test = test[len(pattern[i]):]
+            if path[:len(value[i])] == value[i]:
+                path = path[len(value[i]):]
             else:
-                return False, []
+                return False
         else:
-            if i+1 == len(pattern):
-                content = test
-                test = ''
+            if i+1 == len(value):
+                content = path
+                path = ''
             else:
-                content_end = test.find(pattern[i+1])
+                content_end = path.find(value[i+1])
                 if content_end == -1:
-                    return False, []
+                    return False
                 else:
-                    content = test[:content_end]
-                    test = test[content_end:]
-            func = pattern[i]
+                    content = path[:content_end]
+                    path = path[content_end:]
+            func = value[i]
             try:
-                value = func(content)
+                arg = func(content)
             except ValueError:
-                return False, []
-            args.append(value)
+                return False
+            args.append(arg)
         string = not string
-    if test != '':
-        return False, []
+    if path != '':
+        return False
     else:
-        return True, args
+        router.path_args = args
+        return True
+
+
+PATH = (_check_path, 404)
+
+
+def _check_method(router, environ, value):
+    return value == environ['REQUEST_METHOD']
+
+
+METHOD = (_check_method, 405)
+
+
+def _check_content_type(router, environ, value):
+    if environ.get('CONTENT_TYPE') is None:
+        return value is None
+    elif value is None:
+        return False
+    if '/' in value:
+        return value == environ.get('CONTENT_TYPE', 'text/plain')
+    else:
+        return value in environ.get('CONTENT_TYPE', 'text/plain').split('/')[1].split('+')
+
+
+CONTENT_TYPE = (_check_content_type, 415)
 
 
 class Router:
-    def __init__(self, routes):
+    def __init__(self, rules, routes):
+        self.rules = rules
         self.routes = routes
+        self.path_args = None
 
     def __call__(self, environ, start_response):
-        for method, path in self.routes:
-            if method == environ['REQUEST_METHOD']:
-                match, args = match_pattern(path, environ['PATH_INFO'])
-                if match:
-                    environ['wsgi_tools.routing.args'] = args
-                    return self.routes[(method, path)](environ, start_response)
-
-        raise HTTPException(404)
+        routes = list(self.routes)
+        for i, (rule_func, code) in enumerate(self.rules):
+            routes = [route for route in routes if rule_func(self, environ, route[i])]
+            if len(routes) == 0:
+                self.path_args = None
+                raise HTTPException(code)
+        # for route in self.routes:
+        #     if all(rule(self, environ, value) for rule, value in zip(self.rules, route)):
+        #         response = self.routes[route](environ, start_response)
+        #         self.path_args = None
+        #         return response
+        self.path_args = None
+        return self.routes[routes[0]](environ, start_response)
