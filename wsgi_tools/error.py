7f641955da782829594cbdaed6ff96ce6380bf88
rpkak <rpkak@users.noreply.github.com>
2021-12-27 11:52:41 +0100

    documentation of error.py

--- wsgi_tools/error.py
+++ wsgi_tools/error.py
@@ -1,3 +1,8 @@
+"""Catch any Exception and send, that an error occured.
+
+This is requried by many other modules of this package.
+"""
+
 from abc import ABCMeta, abstractmethod
 from json import dumps
 from sys import exc_info
@@ -7,7 +12,18 @@ from .utils import get_status_code_string, status_codes
 
 
 class HTTPException(Exception):
+    """A Exception which says, what status-code and what message should be displayed
+    """
+
     def __init__(self, code, message=None, exc_info=None, headers=[]):
+        """The cunstructor of HTTPException
+
+        Args:
+            code (int | str): The status-code of this Exception
+            message (str, optional): The message of the error.
+            exc_info (optional): sys.exc_info() if raised because of an other exception.
+            headers (list(tuple), optional): specific headers for this exception
+        """
         Exception.__init__(self)
         self.code = code
         self.message = message
@@ -16,13 +32,26 @@ class HTTPException(Exception):
 
 
 class ErrorHandler(metaclass=ABCMeta):
+    """A WSGI-app which executes another WSGI-App and handles exceptions, if they occure.
+
+    If the exception is no HTTPException, a HTTPException with the code 500, the message
+    `'A server error occurred. Please contact an administrator.'` and the exc_info will be taken.
+
+    This is an abstract class. The handle method needs to be overwritten.
+    """
+
     def __init__(self, app):
+        """The cunstructor of ErrorHandler
+
+        Args:
+            app: The WSGI-app, which is called by the ErrorHandler
+        """
         self.app = app
 
     def __call__(self, environ, start_response):
         try:
             return self.app(environ, start_response)
-        except Exception as e:
+        except BaseException as e:
             if not isinstance(e, HTTPException):
                 print_exc()
                 e = HTTPException(
@@ -35,11 +64,23 @@ class ErrorHandler(metaclass=ABCMeta):
 
     @abstractmethod
     def handle(self, e):
+        """Abstract methed, which has an exception as an arg and returns a body and a list of headers.
+        """
         pass
 
 
 class JSONErrorHandler(ErrorHandler):
+    """An ErrorHandler which returns the error in the json-body.
+    """
+
     def __init__(self, *args, friendly=False, **kwargs):
+        """The cunstructor of ErrorHandler
+
+        Args:
+            friendly (bool, optional): If true, the output will be human readable, else (default), the output will be minimal.
+            *args: The args of ErrorHandler
+            *kwargs: The kwargs of ErrorHandler
+        """
         ErrorHandler.__init__(self, *args, **kwargs)
         self.friendly = friendly
 
@@ -59,6 +100,9 @@ class JSONErrorHandler(ErrorHandler):
 
 
 class HTMLErrorHandler(ErrorHandler):
+    """An ErrorHandler which returns the error in viewable html format.
+    """
+
     def handle(self, e):
         status_code_bytes = get_status_code_string(e.code).encode('utf-8')
         return ([b'<html><head><title>%s</title></head><body><h1>%s</h1>%s</body></html>' % (
