e9c89b11bc8361ca30749384bb1ecbe216a218d3
rpkak <rpkak@users.noreply.github.com>
2021-12-27 12:07:39 +0100

    documentation of basic_auth.py

--- wsgi_tools/basic_auth.py
+++ wsgi_tools/basic_auth.py
@@ -1,10 +1,24 @@
+"""Basic Auth implementation in WSGI.
+"""
+
 from base64 import b64decode
 
 from .error import HTTPException
 
 
 class BasicAuth:
+    """A WSGI-app which asks you to authenticate if you arn't and forwards the request othervise.
+    """
+
     def __init__(self, app, is_correct, realm='Access to content'):
+        """The cunstructor of BasicAuth
+
+        Args:
+            app: The WSGI app with authenticated only access
+            is_correct: a function which processes from the username and the passwd, whether it is
+                correct or wrong.
+            realm (str, optional): What is forbitten without authentication.
+        """
         self.app = app
         self.is_correct = is_correct
         self.realm = realm
