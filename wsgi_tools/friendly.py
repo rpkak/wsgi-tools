1621be9f9894eca50e06ef83ee65fbf3d50abc03
rpkak <rpkak@users.noreply.github.com>
2021-12-30 21:42:53 +0100

    trying out

--- wsgi_tools/friendly.py
+++ wsgi_tools/friendly.py
@@ -8,4 +8,23 @@ class Request:
     """
 
     def __init__(self, environ):
-        pass
+        self.environ = environ
+        self.method = environ['REQUEST_METHOD']
+        self.path = environ['PATH_INFO']
+        self.protocol = environ['SERVER_PROTOCOL']
+        self.query_string = environ.get('QUERY_STRING', '')
+        self.content_type = environ.get('CONTENT_TYPE')
+        self.content_length = int(environ.get('CONTENT_LENGTH') or 0)
+        self.scheme = environ['wsgi.url_scheme']
+        self.body_stream = environ['wsgi.input']
+        self.server = (environ['SERVER_NAME'], environ['SERVER_PORT'])
+        self.headers = {}
+        for key, value in environ.items():
+            if key.startswith('HTTP_'):
+                self.headers[key[5:].replace('_', '-')] = value
+
+    def body_bytes(self) -> bytes:
+        return self.body_stream.read(self.content_length)
+
+    def body_string(self) -> str:
+        return self.body_bytes.decode('utf-8')
