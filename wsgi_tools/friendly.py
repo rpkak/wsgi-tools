c72197d973f7eac52490f51090a534007a2b2b37
rpkak <rpkak@users.noreply.github.com>
2021-12-31 08:28:08 +0100

    trying out

--- wsgi_tools/friendly.py
+++ wsgi_tools/friendly.py
@@ -1,12 +1,3 @@
 """rer
 """
 
-from __future__ import annotations
-
-import xml.etree.ElementTree as ET
-from functools import cached_property
-from json import dumps
-from typing import TYPE_CHECKING
-
-from .utils import get_status_code_string
-
