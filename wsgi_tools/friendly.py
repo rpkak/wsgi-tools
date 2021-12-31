43d4251c3cc3837a9c3fff8b033e8cde80703f66
rpkak <rpkak@users.noreply.github.com>
2021-12-31 08:50:56 +0100

    trying out

--- wsgi_tools/friendly.py
+++ wsgi_tools/friendly.py
@@ -24,9 +24,3 @@ Examples:
 """
 
 from __future__ import annotations
-
-import xml.etree.ElementTree as ET
-from functools import cached_property
-from json import dumps
-
-from .utils import get_status_code_string
