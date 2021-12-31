30d5f52bf371d1c9b5be4b1cb6db1b6baa1872b0
rpkak <rpkak@users.noreply.github.com>
2021-12-31 08:21:05 +0100

    trying out

--- wsgi_tools/friendly.py
+++ wsgi_tools/friendly.py
@@ -123,38 +123,3 @@ def _make_body(body: Union[str, bytes, bytearray]) -> bytes:
         return bytes(body)
     else:
         return body
-
-
-class FriendlyWSGI:
-    """The WSGI-App, which forwards the request to the functions.
-
-    Args:
-        func: the more programmer friendly function.
-    """
-
-    def __init__(self, func: Callable[[Request], Union[Response, tuple[StatusCode, Body], tuple[StatusCode, Body, Headers]]]):
-        self.func = func
-
-    def __call__(self, environ: WSGIEnvironment, start_response: StartResponse) -> Iterable[bytes]:
-        response = self.func(Request(environ))
-
-        if isinstance(response, Response):
-            status, body, headers = response.status, response.body, response.headers
-        elif len(response) == 2:
-            status, body = response
-            headers = {}
-        elif len(response) == 3:
-            status, body, headers = response
-
-        status = get_status_code_string(status)
-
-        if isinstance(headers, dict):
-            headers = [header for header in headers.items()]
-
-        start_response(status, headers)
-
-        if isinstance(body, (str, bytes, bytearray)):
-            body = [_make_body(body)]
-        else:
-            body = (_make_body(d) for d in body)
-        return body
